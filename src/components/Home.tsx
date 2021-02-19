5079ed0aea1a27e99eb0b96afe02dd6ba118de24
piagja <piagja@gmail.com>
2021-02-19 20:28:57 -0300

    Fez o consumo da store e renderizou na tela

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import api from '../services/api'
+import { IUser } from '../store/modules/user/types'
 
 const Home: React.FC = () => {
-  const store = useSelector(state => state)
-  console.log(store)
+  const [ users, setUsers ] = useState<IUser[]>([])
+
+  useEffect(() => {
+    api.get('students').then(
+      response => {
+        setUsers(response.data)
+        console.log(response)
+      }
+    )
+  }, [])
+
   return (
     <div>
       <h1>Hello!!!</h1>
+      {users.map(user => {
+        return (
+        <div key={user.id}>
+          <article>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.age}</p>
+          </article>
+          <hr />
+        </div>
+      )})}
     </div>
   )
 }
